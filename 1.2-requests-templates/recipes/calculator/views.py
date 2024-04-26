9b74e620c389e688468b2876e359b5b6a675868c
Safmaxser <md-86@mail.ru>
2024-04-27 00:53:03 +0700

    completing the task recipes

--- 1.2-requests-templates/recipes/calculator/views.py
+++ 1.2-requests-templates/recipes/calculator/views.py
@@ -16,15 +16,15 @@ DATA = {
         'сыр, ломтик': 1,
         'помидор, ломтик': 1,
     },
-    # можете добавить свои рецепты ;)
 }
 
-# Напишите ваш обработчик. Используйте DATA как источник данных
-# Результат - render(request, 'calculator/index.html', context)
-# В качестве контекста должен быть передан словарь с рецептом:
-# context = {
-#   'recipe': {
-#     'ингредиент1': количество1,
-#     'ингредиент2': количество2,
-#   }
-# }
+def recipes_view(request, recipe):
+    servings = int(request.GET.get('servings', 1))
+    data = dict(DATA.get(recipe, {}))
+    for key in data:
+        data[key] *= servings
+    context = {
+        'name_recipe': recipe,
+        'recipe': data,
+    }
+    return render(request, 'calculator/index.html', context)
