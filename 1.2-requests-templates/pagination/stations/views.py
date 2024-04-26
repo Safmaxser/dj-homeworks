5049352999b95427fa2496678ef3c106e3020d01
Safmaxser <md-86@mail.ru>
2024-04-27 01:37:04 +0700

    execution of task pagination

--- 1.2-requests-templates/pagination/stations/views.py
+++ 1.2-requests-templates/pagination/stations/views.py
@@ -1,3 +1,7 @@
+import csv
+
+from django.conf import settings
+from django.core.paginator import Paginator
 from django.shortcuts import render, redirect
 from django.urls import reverse
 
@@ -7,11 +11,13 @@ def index(request):
 
 
 def bus_stations(request):
-    # получите текущую страницу и передайте ее в контекст
-    # также передайте в контекст список станций на странице
-
+    with open(settings.BUS_STATION_CSV, newline='', encoding='utf-8') as f:
+        reader = list(csv.DictReader(f))
+    paginator = Paginator(reader, 10)
+    current_page = int(request.GET.get('page', 1))
+    stations = paginator.get_page(current_page)
     context = {
-    #     'bus_stations': ...,
-    #     'page': ...,
+        'bus_stations': stations.object_list,
+        'page': stations,
     }
     return render(request, 'stations/index.html', context)
