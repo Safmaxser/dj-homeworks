36102e3fb300b6f5f520310c67be75f22e5f5d44
Safmaxser <md-86@mail.ru>
2024-05-29 22:03:54 +0700

    completed the task Smart Home

--- 3.1-drf-intro/smart_home/measurement/urls.py
+++ 3.1-drf-intro/smart_home/measurement/urls.py
@@ -1,5 +1,9 @@
 from django.urls import path
 
+from .views import SensorsListView, SensorView, MeasurementsView
+
 urlpatterns = [
-    # TODO: зарегистрируйте необходимые маршруты
+    path('sensors/', SensorsListView.as_view()),
+    path('sensors/<int:pk>/', SensorView.as_view()),
+    path('measurements/', MeasurementsView.as_view()),
 ]
--- 3.1-drf-intro/smart_home/measurement/views.py
+++ 3.1-drf-intro/smart_home/measurement/views.py
@@ -1,2 +1,26 @@
-# TODO: опишите необходимые обработчики, рекомендуется использовать generics APIView классы:
-# TODO: ListCreateAPIView, RetrieveUpdateAPIView, CreateAPIView
+from rest_framework.generics import get_object_or_404
+from rest_framework.generics import ListCreateAPIView
+from rest_framework.generics import RetrieveUpdateDestroyAPIView
+
+from .models import Sensor, Measurement
+from .serializers import SensorSerializer, SensorDetailSerializer
+from .serializers import MeasurementSerializer
+
+
+class SensorsListView(ListCreateAPIView):
+    queryset = Sensor.objects.all()
+    serializer_class = SensorSerializer
+
+
+class SensorView(RetrieveUpdateDestroyAPIView):
+    queryset = Sensor.objects.all()
+    serializer_class = SensorDetailSerializer
+
+
+class MeasurementsView(ListCreateAPIView):
+    queryset = Measurement.objects.all()
+    serializer_class = MeasurementSerializer
+
+    def perform_create(self, serializer):
+        sensor = get_object_or_404(Sensor, id=self.request.data.get('sensor'))
+        return serializer.save(sensor=sensor)
